7a87afb09fb4a1ca79995bb07ef34258287579a7
Nikhil <nchagato@buffalo.edu>
2018-10-03 08:19:57 -0400

    added index and script.js

---
+++ Basics/script.js
@@ -0,0 +1,9 @@
+var firstNname = 'Nikhil'
+console.log("Hello " + firstNname);
+
+var lastName = 'Chagatoor Rudramuni Gowda';
+console.log('Your last name is '+lastName);
+
+var age = 28;
+var fullAge = true;
+console.log(fullAge);
